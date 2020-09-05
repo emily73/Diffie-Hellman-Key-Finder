ca1fe84e209d1cdaf6f4be5231e8cc9ba6c5f3d3
Emily <66376925+emily73@users.noreply.github.com>
2020-09-05 12:26:57 +0100

    Create DHsecretkeyfinder.py

---
+++ DHsecretkeyfinder.py
@@ -0,0 +1,12 @@
+def Input_Numbers():
+  g = int(input("Enter the Generator Number: "))
+  n = int(input("Enter the Prime Number:"))
+  priv_a = int(input("Enter the Alice's private key: "))
+  priv_b = int(input("Enter the Bob's private key: "))
+  pub_a = g**priv_a % n
+  pub_b = g**priv_b % n
+  k = pow(g,priv_a*priv_b,n)
+  print("------------------------------------")
+  print("Alice's public key is: ",pub_a)
+  print("Bob's public key is: ", pub_b)
+  print("The Secret Key is: ",k)
